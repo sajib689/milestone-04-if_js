33b0d480433635e75a027fcc9186e98f3a02a29e
sajib689 <jibonraja11@gmail.com>
2024-01-27 17:35:44 +0600

    ternat

--- task.js
+++ task.js
@@ -17,3 +17,20 @@ if(num > 100){
 } else {
     console.log('you got F')
 }
+let x = 5; 
+if (x < 10) 
+{ x = x + 3; } 
+else { x = x - 2 }
+
+console.log(x);
+let result = 10 < "5";
+console.log(result);
+console.log(6 != 6);
+let score = 75;
+let grade;
+if (score >= 90) {
+  grade = "A";
+} else if (score >= 80) {
+  grade = "B";
+}
+console.log(grade);
