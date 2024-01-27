53a16250a7e29a100a9b203759c41e0e65d9edbf
sajib689 <jibonraja11@gmail.com>
2024-01-27 17:13:57 +0600

    ternat

---
+++ task.js
@@ -0,0 +1,19 @@
+const price = 500
+
+if(price <= 600){
+    console.log('free coke')
+} else {
+    console.log('pay for this coke')
+}
+
+const num = 90
+
+if(num > 100){
+    console.log('you got A')
+} else if (num > 80){
+    console.log('you got B')
+} else if (num > 60){
+    console.log('you got C')
+} else {
+    console.log('you got F')
+}
