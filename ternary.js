5af2c6c3204b080880bb3ad94ca77ecc5402af14
sajib689 <jibonraja11@gmail.com>
2024-01-27 12:18:12 +0600

    ternat

---
+++ ternary.js
@@ -0,0 +1,8 @@
+// Ternary operator
+const age = 20
+if(age >= 18) {
+    console.log('you can vote')
+} else {
+    console.log('you can not vote')
+}
+console.log(age >= 18 ? 'You can vote' : 'You can not vote')
